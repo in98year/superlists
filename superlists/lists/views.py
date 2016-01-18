21e082060342bbfc848870696130686ce4e1a0dd
m516 <m516@m516>
2016-01-18 16:03:37 +0800

    重構homePage view並使用範本

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+
 
 # Create your views here.
 
 def homePage(request):
-    return HttpResponse('<!doctype html><html><head><title>待辦事項清單</title></head><body></body></html>')
+    return render(request, 'lists/home.html')
