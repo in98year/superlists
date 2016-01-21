dcd2a331e92591a3584624ce0a16ffe28761011c
m516 <m516@m516>
2016-01-21 14:10:08 +0800

    新的URL + 新增項目到清單。FT通過 ;-)

--- superlists/lists/urls.py
+++ superlists/lists/urls.py
@@ -4,6 +4,7 @@ from lists import views
  
 urlpatterns = [
     url(r'^$', views.homePage, name='homePage'),
-    url(r'^the-only-list-in-the-world/$', views.viewList, name='viewList'),
+    url(r'^(?P<listID>[0-9]+)/$', views.viewList, name='viewList'),
     url(r'^new/$', views.newList, name='newList'),
+    url(r'(?P<listID>[0-9]+)/addItem/', views.addItem, name='addItem'),
 ]
