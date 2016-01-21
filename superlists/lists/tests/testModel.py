8fa75d6533272b1ad6c7e04acfcb2e31ede70f83
m516 <m516@m516>
2016-01-21 15:24:04 +0800

    將單元測試分為2個檔案

---
+++ superlists/lists/tests/testModel.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from lists.models import Item, List
+
+# Create your tests here. 
+class ListAndItemModelTest(TestCase):
+    
+    
+    def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+        
+        firstItem = Item()
+        firstItem.text = '第一個清單項目'
+        firstItem.list = list_
+        firstItem.save()
+        
+        secondItem = Item()
+        secondItem.text = '第二個清單項目'
+        secondItem.list = list_
+        secondItem.save()
+        
+        savedList = List.objects.first()
+        self.assertEqual(savedList, list_)
+        
+        savedItems = Item.objects.all()
+        self.assertEqual(savedItems.count(), 2)
+        
+        firstSavedItem = savedItems[0]
+        secondSavedItem = savedItems[1]
+        self.assertEqual(firstSavedItem.text, '第一個清單項目')
+        self.assertEqual(firstSavedItem.list, list_)
+        self.assertEqual(secondSavedItem.text, '第二個清單項目')
+        self.assertEqual(secondSavedItem.list, list_)
+   
+        
--- superlists/lists/tests/testView.py
+++ superlists/lists/tests/testView.py
@@ -1,7 +1,5 @@
-from django.http import HttpRequest
 from django.core.urlresolvers import resolve, reverse
 from django.test import TestCase
-from django.shortcuts import render
 from lists.views import homePage
 from lists.models import Item, List
 
@@ -15,36 +13,7 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, homePage)
 
         
-class ListAndItemModelTest(TestCase):
-    
-    
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-        
-        firstItem = Item()
-        firstItem.text = '第一個清單項目'
-        firstItem.list = list_
-        firstItem.save()
-        
-        secondItem = Item()
-        secondItem.text = '第二個清單項目'
-        secondItem.list = list_
-        secondItem.save()
-        
-        savedList = List.objects.first()
-        self.assertEqual(savedList, list_)
-        
-        savedItems = Item.objects.all()
-        self.assertEqual(savedItems.count(), 2)
-        
-        firstSavedItem = savedItems[0]
-        secondSavedItem = savedItems[1]
-        self.assertEqual(firstSavedItem.text, '第一個清單項目')
-        self.assertEqual(firstSavedItem.list, list_)
-        self.assertEqual(secondSavedItem.text, '第二個清單項目')
-        self.assertEqual(secondSavedItem.list, list_)
-   
+
         
 class ListViewTest(TestCase):
     
