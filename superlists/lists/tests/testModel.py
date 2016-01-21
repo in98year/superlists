2aac0567956ef10ae8f1c8da5b8642028aab4998
m516 <m516@m516>
2016-01-21 15:36:47 +0800

    1/21 finish

--- superlists/lists/tests/testModel.py
+++ superlists/lists/tests/testModel.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.models import Item, List
+from django.core.exceptions import ValidationError
 
 # Create your tests here. 
 class ListAndItemModelTest(TestCase):
@@ -31,5 +32,13 @@ class ListAndItemModelTest(TestCase):
         self.assertEqual(firstSavedItem.list, list_)
         self.assertEqual(secondSavedItem.text, '第二個清單項目')
         self.assertEqual(secondSavedItem.list, list_)
+        
+        
+    def test_cannot_save_empty_list_items(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean() 
    
         
